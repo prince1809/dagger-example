09957189da6aa23755dda202a34b85cfa5b2dc0b
prince <prince18pat@gmail.com>
2019-07-09 00:46:20 +0900

    add starter files

---
+++ src/main/java/coffee/CoffeeApp.java
@@ -0,0 +1,11 @@
+package coffee;
+
+/**
+ * Created by prince on 2019/07/09.
+ */
+public class CoffeeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Brewing coffee......");
+    }
+}
